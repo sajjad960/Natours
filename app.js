8d42957d1d28b1f3d3a15a4ac48baf14d06ec41b
sajjad960 <sajjadhossainsb01@gmail.com>
2022-02-24 22:44:38 +0600

    xss protection and data sanitization protection, and validator also take many schema protection on it's own

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const tourRouter = require('./routs/tourRouters');
 const tourUser = require('./routs/userRouters');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const app = express();
 
@@ -32,8 +34,10 @@ app.use('/api', limiter);
 app.use(express.json({ limit: '10kb' }));
 
 //Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
 
 // Data sanitization against XSS
+app.use(xss());
 
 app.use(express.static('./public'));
 
