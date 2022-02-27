c7257917f31b78e810bda5252b9c5a7dc79e7f17
sajjad960 <sajjadhossainsb01@gmail.com>
2022-02-27 19:16:51 +0600

    added child ref for guide with User and take this id into DB

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const User = require('./userModel')
+// const User = require('./userModel');
 // const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
@@ -96,7 +96,12 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
-    guides: Array,
+    guides: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+      },
+    ],
   }
   // {
   //   toJson: { virtuals: true },
@@ -105,15 +110,15 @@ const tourSchema = new mongoose.Schema(
 );
 
 // DOCUMENT MIDDLEWARE: runs before .save and .create() //not gonna work for insertMany
-tourSchema.pre('save', function () {
-  console.log(this);
-});
+// tourSchema.pre('save', function () {
+//   console.log(this);
+// });
 
-tourSchema.pre('save', async function (next) {
-  const guidesPromises = this.guides.map(async (id) => await User.findById(id));
-  this.guides = await Promise.all(guidesPromises);
-  next();
-});
+// tourSchema.pre('save', async function (next) {
+//   const guidesPromises = this.guides.map(async (id) => await User.findById(id));
+//   this.guides = await Promise.all(guidesPromises);
+//   next();
+// });
 
 tourSchema.pre('find', function (next) {
   this.find({ secretTour: { $ne: true } });
