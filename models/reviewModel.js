afea2fab9cac434acc4a63590469264c3f6e172c
sajjad960 <sajjadhossainsb01@gmail.com>
2022-03-12 23:18:28 +0600

    learning schema indexing

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,35 +1,42 @@
 const mongoose = require('mongoose');
 
-const reviewSchema = new mongoose.Schema({
-  review: String,
-  rating: Number,
-  createdAt: Date.now(),
-  tour: [
-    {
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'Review can not be empty!']
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    tour: {
       type: mongoose.Schema.ObjectId,
       ref: 'Tour',
-      required: [true, 'Review must belong to a tour'],
+      required: [true, 'Review must belong to a tour.'],
     },
-  ],
-  user: [
-    {
+    user: {
       type: mongoose.Schema.ObjectId,
-      ref: 'Users',
+      ref: 'User',
       required: [true, 'Review must belong to a user'],
-    },
-  ],
- },
+    }
+  },
   {
-    toJson: { virtuals: true },
-    toObject: { virtuals: true },
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
   }
 );
 
-reviewSchema.pre(/^find/, function(next){
+reviewSchema.pre(/^find/, function (next) {
   this.populate({
     path: 'users',
     select: 'name photo',
-})
+  })
 })
 
 const Review = mongoose.model('Tour', reviewSchema);
